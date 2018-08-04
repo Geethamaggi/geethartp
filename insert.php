c4dd6724d857ddf15d80cbf19bff83d97c4231db
Geethachandrasekar <geethac323@gmail.com>
2018-08-04 14:59:31 +0530

    done

---
+++ insert.php
@@ -0,0 +1,19 @@
+<?php
+
+
+	include 'conn.php';
+	$name = $_POST['name'];
+	$email = $_POST['email'];
+	$password =$_POST['password'];
+	$password =password_hash($password, PASSWORD_BCRYPT);
+
+	
+	$query="INSERT INTO `register`(`name`,`email`, `password`) VALUES ('$name','$email','$password')";
+	$result=mysqli_query($con,$query);
+	if ($result) {
+		header("location:test.html");
+	}
+	else{
+		echo "Error";
+	}
+?>
